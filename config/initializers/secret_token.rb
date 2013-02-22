3fe6515b1a5f4636c4a48d13379006ea263ec06b
Ubuntu <ubuntu@muncher-on-cirrus-1.novalocal>
2013-02-22 14:55:29 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuncherOnCirrus::Application.config.secret_token = 'bfd13bb563e127c21b76d49532917a089a72cad5541cbeb2a75a8047344e9d61260cb25f3dcc3a26420dcdad436d08cd8208cf7c03d96b61df6e0297bda3c305'
